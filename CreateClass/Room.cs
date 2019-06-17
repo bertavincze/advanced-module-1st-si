72ec9cd61a52c578f35f5b4158c37a8506a8dc80
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 10:35:03 +0200

    Add Room class

---
+++ CreateClass/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Room
+    {
+        int number;
+
+        public Room(int number)
+        {
+            this.number = number;
+        }
+
+        public int getNumber()
+        {
+            return number;
+        }
+    }
+}

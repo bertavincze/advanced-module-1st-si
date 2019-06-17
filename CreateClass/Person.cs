152e3be4c6ab1fced89570c3477551b80abf6c06
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 10:08:48 +0200

    Create Person class

---
+++ CreateClass/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public enum Gender { Male, Female };
+
+    class Person
+    {
+        string name;
+        DateTime birthdate;
+        Gender gender;
+
+        public Person(string name, DateTime birthdate, Gender gender)
+        {
+            this.name = name;
+            this.birthdate = birthdate;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + this.name + "\nBirthdate: " + this.birthdate + "\nGender: " + this.gender;
+        }
+    }
+}
---
+++ CreateClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Béla", DateTime.Now, Gender.Male);
+            Console.WriteLine(person.ToString());
+            Console.ReadKey();
+        }
+    }
+}

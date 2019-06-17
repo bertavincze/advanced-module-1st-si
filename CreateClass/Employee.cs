8ab3a63b9e79ce8c34c6f8f5cc727c9f08fc8022
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 10:33:54 +0200

    Add Employee class

---
+++ CreateClass/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Employee : Person
+    {
+        int salary;
+        string profession;
+        Room room;
+
+        public Employee(string name, DateTime birthdate, Gender gender, int salary, string profession, Room room) : base(name, birthdate, gender)
+        {
+            this.salary = salary;
+            this.profession = profession;
+            this.room = room;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nSalary: " + salary + "\nProfession: " + profession + "\nRoom: " + room.getNumber();
+        }
+    }
+}
